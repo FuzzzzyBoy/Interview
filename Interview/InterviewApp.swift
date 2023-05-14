214ad5537bdfd59bcd4bff88e2788a1fc49cae78
Kirill Shakhansky <k.shakhanskiy@uzum.com>
2023-05-14 12:50:30 +0500

    Initial Commit

---
+++ Interview/InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewApp.swift
+//  Interview
+//
+//  Created by Kirill Shahanskiy on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
