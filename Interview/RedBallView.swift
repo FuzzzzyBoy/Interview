00001038a61c64c5d94713a96c89157c65f7ced9
Kirill Shakhansky <k.shakhanskiy@uzum.com>
2023-05-14 13:09:29 +0500

    add red ball view

---
+++ Interview/RedBallView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct RedBallView: View {
+	
+	@State var toggle = true
+	
+	var body: some View {
+		HStack {
+			Button("PUSH ME") {
+				toggle.toggle()
+			}
+			.font(.largeTitle)
+			.padding(.trailing, 50)
+			
+			if toggle {
+				Color.red
+					.frame(width: 50, height: 50)
+					.clipShape(Circle())
+			} else {
+				Color.red
+					.frame(width: 50, height: 50)
+					.clipShape(Circle())
+					.padding(.bottom, 300)
+			}
+		}
+		.padding()
+	}
+	
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		RedBallView()
+	}
+}
