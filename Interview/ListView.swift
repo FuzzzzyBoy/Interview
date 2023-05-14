e298c3460c3c08320d07eb3bce4ba3e52307545a
Kirill Shakhansky <k.shakhanskiy@uzum.com>
2023-05-14 13:29:56 +0500

    add ListView

---
+++ Interview/ListView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct ListView: View {
+	
+	var numbers = [1, 2, 3]
+	var letters = ["A", "B", "C"]
+
+	var body: some View {
+		VStack {
+			List {
+				Section {
+					ForEach(numbers, id: \.self) { number in
+						Text("\(number)")
+					}
+				}
+				
+				Section {
+					ForEach(letters, id: \.self) { letter in
+						Text(letter)
+					}
+				}
+			}
+			
+			Button("PUSH ME") {
+				let first = numbers.removeFirst()
+				letters.insert("\(first)", at: 0)
+			}
+			.font(.largeTitle)
+		}
+	}
+
+}
+
+struct ListView_Previews: PreviewProvider {
+	static var previews: some View {
+		ListView()
+	}
+}
